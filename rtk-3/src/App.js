cd6edcfbfa1073f930d90251d18e411c6893e5a9
1frstn <krmilhn@icloud.com>
2023-07-24 13:57:12 +1000

    3paart updated

--- rtk-3/src/App.js
+++ rtk-3/src/App.js
@@ -1,9 +1,12 @@
 import PostsList from "./features/posts/PostsList";
 import AddPostForm from "./features/posts/AddPostForm";
 import Layout from "./componant/Layout";
-import { Routes,Route } from "react-router-dom";
+import { Routes,Route, Navigate } from "react-router-dom";
 import SinglePostPage from "./features/posts/SinglePostPage";
 import EditPostForm from "./features/posts/EditPostForm";
+import UsersList from "./features/users/UsersList";
+import UserPage from "./features/users/UserPage";
+
 
 function App() {
   return (
@@ -15,6 +18,12 @@ function App() {
           <Route path=":postId" element={<SinglePostPage/>} />
           <Route path="edit/:postId" element={<EditPostForm/>} />
         </Route>
+        <Route path="user" >
+           <Route index element={<UsersList/>}/>
+           <Route path=":userId" element={<UserPage/>} />
+        </Route>
+        {/* Catch all replace with 404 component if you want */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
     </Routes>
   );
--- rtk-3/src/features/posts/PostsExcerpt.js
+++ rtk-3/src/features/posts/PostsExcerpt.js
@@ -1,9 +1,40 @@
 import PostAuthor from "./PostAuthor";
 import TimeAgo from "./TimeAgo";
 import ReactionButtons from "./ReactionButtons";
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-const PostsExcerpt = ({ post }) => {
+import { useSelector } from "react-redux";
+import { selectPostById } from "./postsSlice";
+
+const PostsExcerpt = ({ postId }) => {
+    const post = useSelector(state => selectPostById(state, postId))
+
+    return (
+        <article>
+            <h2>{post.title}</h2>
+            <p className="excerpt">{post.body.substring(0, 75)}...</p>
+            <p className="postCredit">
+                <Link to={`post/${post.id}`}>View Post</Link>
+                <PostAuthor userId={post.userId} />
+                <TimeAgo timestamp={post.date} />
+            </p>
+            <ReactionButtons post={post} />
+        </article>
+    )
+}
+
+export default PostsExcerpt
+
+
+/* React.memo() */
+/* 
+ import PostAuthor from "./PostAuthor";
+import TimeAgo from "./TimeAgo";
+import ReactionButtons from "./ReactionButtons";
+import { Link } from "react-router-dom";
+import React from "react";
+
+let PostsExcerpt = ({ post }) => {
     return (
         <article>
             <h2>{post.title}</h2>
@@ -17,4 +48,9 @@ const PostsExcerpt = ({ post }) => {
         </article>
     )
 }
+
+PostsExcerpt = React.memo(PostsExcerpt)
+
 export default PostsExcerpt
+
+*/
